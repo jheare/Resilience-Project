9462a83b9796e1d7f6a91137670dc4b87b004f0e
jheare <jheare@uw.edu>
2015-07-01 21:06:41 -0700

    update

--- qPCR data/raw fluoro/fluorotoCtscript.R
+++ qPCR data/raw fluoro/fluorotoCtscript.R
@@ -1,5 +1,6 @@
 require(qpcR)
 require(plyr)
+require(ggplot2)
 
 craf3<-read.csv("CRAF3rawfluoro.csv", header = T)
 craf3$X<-NULL
@@ -20,3 +21,25 @@ craf3<-rename(craf3, c("Cycle" = "Cycles", "A1" = "HC1", "A2" = "NC1",
 
 craf3ct<-pcrbatch(craf3, fluo=NULL)
 write.csv(craf3ct, file="CRAF3ct.csv", row.names=F)
+
+
+test <- setNames(data.frame(t(craf3ct)),craf3ct[,1])
+test<-test[-1,]
+
+ggplot(test, aes(x=rownames(test),y=sig.cpD2, fill=rownames(test)))+geom_bar(stat="identity")
+
+
+craf3<-rename(craf3, c("Cycle" = "Cycles", "A1" = "HC", "A2" = "NC",
+                       "A3"= "SC", "A4"="HT", "A5"="NT","A6"="ST",
+                       "A7"="NTC","B1" = "HC", "B2" = "NC","B3"= "SC",
+                       "B4"="HT", "B5"="NT", "B6"="ST","B7"="NTC",
+                       "C1" = "HC", "C2" = "NC","C3"= "SC","C4"="HT",
+                       "C5"="NT", "C6"="ST", "C7"="NTC","D1" = "HC",
+                       "D2" = "NC","D3"= "SC", "D4"="HT", "D5"="NT",
+                       "D6"="ST", "D7"="NTC","E1" = "HC", "E2" = "NC",
+                       "E3"= "SC", "E4"="HT", "E5"="NT", "E6"="ST",
+                       "F1" = "HC", "F2" = "NC","F3"= "SC", "F4"="HT",
+                       "F5"="NT", "F6"="ST","G1" = "HC", "G2" = "NC",
+                       "G3"= "SC", "G4"="HT", "G5"="NT", "G6"="ST",
+                       "H1" = "HC", "H2" = "NC","H3"= "SC", "H4"="HT",
+                       "H5"="NT", "H6"="ST"))
